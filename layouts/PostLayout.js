0d43a895be06ee458ef0cdaa8461bbf57f067602
siddharth sinha <siddharth0sinha@gmail.com>
2021-06-29 01:41:34 +0530

    v2

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -29,63 +29,11 @@ export default function PostLayout({ children, frontMatter, next, prev }) {
             className="pb-8 divide-y divide-gray-200 xl:divide-y-0 dark:divide-gray-700 3xl:grid 3xl:grid-cols-4 3xl:gap-x-6"
             style={{ gridTemplateRows: 'auto 1fr' }}
           >
-            {/* <dl className="pt-6 pb-10 xl:pt-11 xl:border-b xl:border-gray-200 xl:dark:border-gray-700">
-              <dt className="sr-only">Authors</dt>
-              <dd>
-                <ul className="flex justify-center space-x-8 xl:block sm:space-x-12 xl:space-x-0 xl:space-y-8">
-                  <li className="flex items-center space-x-2">
-                    <Image
-                      src={siteMetadata.image}
-                      width="38px"
-                      height="38px"
-                      alt="avatar"
-                      className="w-10 h-10 rounded-full"
-                    />
-                    <dl className="text-sm font-medium leading-5 whitespace-nowrap">
-                      <dt className="sr-only">Name</dt>
-                      <dd className="text-gray-900 dark:text-gray-100">{siteMetadata.author}</dd>
-                      {typeof siteMetadata.twitter === 'string' && (
-                        <>
-                          <dt className="sr-only">Twitter</dt>
-                          <dd>
-                            <Link
-                              href={siteMetadata.twitter}
-                              className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
-                            >
-                              {siteMetadata.twitter.replace('https://twitter.com/', '@')}
-                            </Link>
-                          </dd>
-                        </>
-                      )}
-                    </dl>
-                  </li>
-                </ul>
-              </dd>
-            </dl> */}
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:pb-0 xl:col-span-3 xl:row-span-2">
               <div className="pt-10 pb-8 prose dark:prose-dark max-w-none">{children}</div>
-              {/* <div className="pt-6 pb-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={discussUrl(slug)} rel="nofollow">
-                  {'Discuss on Twitter'}
-                </Link>
-                {` • `}
-                <Link href={editUrl(fileName)}>{'View on GitHub'}</Link>
-              </div> */}
             </div>
             <footer>
               <div className="text-sm font-medium leading-5 divide-gray-200 xl:divide-y dark:divide-gray-700 xl:col-start-1 xl:row-start-2">
-                {tags && (
-                  <div className="py-4 xl:py-8">
-                    <h2 className="text-xs tracking-wide text-gray-500 uppercase dark:text-gray-400">
-                      Tags
-                    </h2>
-                    <div className="flex flex-wrap">
-                      {tags.map((tag) => (
-                        <Tag key={tag} text={tag} />
-                      ))}
-                    </div>
-                  </div>
-                )}
                 {(next || prev) && (
                   <div className="flex justify-between py-4 xl:block xl:space-y-8 xl:py-8">
                     {prev && (
